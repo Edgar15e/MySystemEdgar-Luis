eac7c10613cd03b1f5618b491ed4ad0af12447ca
Luis Alberto <luigigol_campeon@hotmail.com>
2019-09-20 19:28:13 -0500

    //enums2.0

--- Debouncer_practica1/source/GPIO_Driver.c
+++ Debouncer_practica1/source/GPIO_Driver.c
@@ -4,6 +4,24 @@
 /**
 assign the Botton ports
 **/
+typedef enum{
+	enBotton0=0,
+	enBotton1,
+	enBotton2,
+	enBotton3,
+	enBotton4,
+	enBotton5,
+	enBottoms
+}MyBottoms;
+
+typedef enum{
+	enDisplay1=10,
+	enDisplay2,
+	enDisplay3,
+	enDisplay4,
+	enDisplays
+}MyDisplays;
+
 
 void GPIOS (void){
 	SIM->SCGC5|=PUERTOSE;//E
@@ -11,58 +29,58 @@ void GPIOS (void){
 	SIM->SCGC5|=PUERTOSC;//C
 	SIM->SCGC5|=PUERTOSA;//A
 
-	PORTE->PCR[0]|= ON;
-	PORTE->PCR[1]|= ON;
-	PORTE->PCR[2]|= ON;
-	PORTE->PCR[3]|= ON;
-	PORTE->PCR[4]|= ON;
-	PORTE->PCR[5]|= ON;
+	PORTE->PCR[enBotton0]|= ON;
+	PORTE->PCR[enBotton1]|= ON;
+	PORTE->PCR[enBotton2]|= ON;
+	PORTE->PCR[enBotton3]|= ON;
+	PORTE->PCR[enBotton4]|= ON;
+	PORTE->PCR[enBotton5]|= ON;
 
-	PORTC->PCR[10]|= ON;
-	PORTC->PCR[11]|= ON;
-	PORTC->PCR[12]|= ON;
-	PORTC->PCR[13]|= ON;
+	PORTC->PCR[enDisplay1]|= ON;
+	PORTC->PCR[enDisplay2]|= ON;
+	PORTC->PCR[enDisplay3]|= ON;
+	PORTC->PCR[enDisplay4]|= ON;
 
-	PORTA->PCR[2]|= ON;
+	PORTD->PCR[0]|= ON;
 	PORTA->PCR[1]|= ON;
+	PORTA->PCR[2]|= ON;
+	PORTD->PCR[3]|= ON;
 	PORTA->PCR[12]|=ON;
 	PORTA->PCR[5]|= ON;
-	PORTD->PCR[0]|= ON;
 	PORTD->PCR[2]|= ON;
-	PORTD->PCR[3]|= ON;
 
-	GPIOE->PDDR&=~(1<<0);
-	GPIOE->PDDR&=~(1<<1);
-	GPIOE->PDDR&=~(1<<2);
-	GPIOE->PDDR&=~(1<<3);
-	GPIOE->PDDR&=~(1<<4);
-	GPIOE->PDDR&=~(1<<5);
+	GPIOE->PDDR&=~enBotton0;
+	GPIOE->PDDR&=~enBotton1;
+	GPIOE->PDDR&=~enBotton2;
+	GPIOE->PDDR&=~enBotton3;
+	GPIOE->PDDR&=~enBotton4;
+	GPIOE->PDDR&=~enBotton5;
 
-	GPIOE->PDDR|=(1<<10);
-	GPIOE->PDDR|=(1<<11);
-	GPIOE->PDDR|=(1<<12);
-	GPIOE->PDDR|=(1<<13);
+	GPIOE->PDDR|=enDisplay1;//(1<<10)
+	GPIOE->PDDR|=enDisplay2;//(1<<11)
+	GPIOE->PDDR|=enDisplay3;//(1<<12)
+	GPIOE->PDDR|=enDisplay4;//(1<<13)
 
-	GPIOA->PDDR|= (1<<2);
-	GPIOA->PDDR|= (1<<1);
-	GPIOA->PDDR|= (1<<12);
-	GPIOA->PDDR|= (1<<5);
-	GPIOD->PDDR|= (1<<0);
-	GPIOD->PDDR|= (1<<2);
-	GPIOD->PDDR|= (1<<3);
+	GPIOA->PDDR|= SegmentoA;//enSegmentoA
+	GPIOA->PDDR|= SegmentoB;//enSegmentoB
+	GPIOA->PDDR|= SegmentoC;//enSegmentoC
+	GPIOA->PDDR|= SegmentoD;//enSegmentoD
+	GPIOD->PDDR|= SegmentoE;// enSegmentoE
+	GPIOD->PDDR|= SegmentoF;//enSegmentoF
+	GPIOD->PDDR|= SegmentoG;//enSegmentoG
 
-	GPIOC->PSOR|=(1<<10);
-	GPIOC->PSOR|=(1<<11);
-	GPIOC->PSOR|=(1<<12);
-	GPIOC->PSOR|=(1<<13);
+	GPIOC->PSOR|=enDisplay1;
+	GPIOC->PSOR|=enDisplay2;
+	GPIOC->PSOR|=enDisplay3;
+	GPIOC->PSOR|=enDisplay4;
 
-	GPIOA->PSOR|= (1<<2);
-	GPIOA->PSOR|= (1<<1);
-	GPIOA->PSOR|= (1<<12);
-	GPIOA->PSOR|= (1<<5);
-	GPIOD->PSOR|= (1<<0);
-	GPIOD->PSOR|= (1<<2);
-	GPIOD->PSOR|= (1<<3);
+	GPIOA->PSOR|= SegmentoA;
+	GPIOA->PSOR|= SegmentoB;
+	GPIOA->PSOR|= SegmentoC;
+	GPIOA->PSOR|= SegmentoD;
+	GPIOD->PSOR|= SegmentoE;
+	GPIOD->PSOR|= SegmentoF;
+	GPIOD->PSOR|= SegmentoG;
 }
 
 
--- Debouncer_practica1/source/GPIO_Driver.h
+++ Debouncer_practica1/source/GPIO_Driver.h
@@ -8,6 +8,14 @@
 #define PUERTOSC (1<<11)
 #define PUERTOSA (1<<9)
 
+#define SegmentoA (1<<2)
+#define SegmentoB (1<<1)
+#define SegmentoC (1<<12)
+#define SegmentoD (1<<5)
+#define SegmentoE (1<<0)
+#define SegmentoF (1<<2)
+#define SegmentoG (1<<3)
+
 #define ON (1<<8)
 
 
